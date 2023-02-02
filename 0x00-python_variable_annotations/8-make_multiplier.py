2e763ad873576328ff09be760b59488dc83ea3db
lola-source <99507617+lola-source@users.noreply.github.com>
2023-02-02 23:31:59 +0300

    Update 8-make_multiplier.py

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
-""" Complex types - functions """
+"""
+    Callable function
+"""
 from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    """ type-annotated function make_multiplier that takes a float multiplier
-        as argument and returns a function that multiplies a float by
-        multiplier. """
-    def fn(n: float):
-        return n * multiplier
-    return fn
+    """
+        Args:
+            multiplier: factor
+        Return:
+            multiplication in float
+    """
+
+    def x(f: float) -> float:
+        """ Get the second argument somthing like JS """
+        return float(f * multiplier)
+
+    return x
