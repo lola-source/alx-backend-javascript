6850270eb655c24c999ffcc0c914c8882deb5cd7
lola-source <99507617+lola-source@users.noreply.github.com>
2023-02-02 23:18:57 +0300

    Update 101-safely_get_value.py

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python3
-""" Given the parameters and the return values,
-    add type annotations to the function
-    Hint: look into TypeVar
-    def safely_get_value(dct, key, default = None):
-        if key in dct:
-            return dct[key]
-        else:
-            return default """
-from typing import Mapping, Any, Union, TypeVar
+"""
+    Duck typing Typevar
+"""
+from typing import Mapping, TypeVar, Any, Union
+
+
 T = TypeVar('T')
 
 
-def safely_get_value(
-        dct: Mapping,
-        key: Any,
-        default: Union[T, None] = None
-        ) -> Union[Any, T]:
-    """ More involved type annotations """
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None)\
+        -> Union[Any, T]:
+    """
+        Args:
+            dct: Mapping
+            key: Any data type
+            default: Default value
+        Return:
+            Any or T format
+    """
     if key in dct:
         return dct[key]
     else:
