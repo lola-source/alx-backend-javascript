4a7b18ee8ff65ccf1fd00cdfb485e97027ce6c3f
lola-source <99507617+lola-source@users.noreply.github.com>
2023-02-02 23:24:47 +0300

    Update 4-define_variables.py

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
-""" Define variables
-    Define and annotate the following variables with the specified values """
+"""
+    Variables to annotate
+"""
+
+
 a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
-school: str = 'Holberton'
+school: str = "Holberton"
