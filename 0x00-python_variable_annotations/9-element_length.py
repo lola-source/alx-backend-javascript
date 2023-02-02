3bd21a80d88a9332915028f94a715b0ee9e0b243
lola-source <99507617+lola-source@users.noreply.github.com>
2023-02-02 23:32:43 +0300

    Update 9-element_length.py

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
-""" Annotate the below function’s parameters and
-    return values with the appropriate types.
-    def element_length(lst):
-        return [(i, len(i)) for i in lst] """
-from typing import Iterable, Sequence, List, Tuple
+"""
+    Duck type and iteration
+"""
+from typing import Iterable, Sequence, List, Union, Tuple
 
 
-def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    """ Let's duck type an iterable object """
+def element_length(lst: Iterable[Sequence])\
+        -> List[Tuple[Sequence, int]]:
+    """
+        Args:
+            lst: Sequence of list
+        Return:
+            List of tuple of sequence of integers
+    """
+
     return [(i, len(i)) for i in lst]
