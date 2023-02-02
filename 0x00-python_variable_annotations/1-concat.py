e1632cae431b558d93dd0ce475fa118c3ce13faf
lola-source <99507617+lola-source@users.noreply.github.com>
2023-02-02 23:13:13 +0300

    Update 1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
-""" Basic annotations - concat """
+"""
+    Handle of string with annotations
+"""
 
 
 def concat(str1: str, str2: str) -> str:
-    """ type-annotated function concat that takes a string str1 and a string
-        str2 as arguments and returns a concatenated string """
-    return str1 + str2
+    """
+        Args:
+            str1: The first string parameter
+            str1: The second string parameter
+        Return:
+            Concatenated string
+    """
+
+    return (str1 + str2)
