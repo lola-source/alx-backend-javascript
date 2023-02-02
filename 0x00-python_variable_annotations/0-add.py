5daa101ae6100b368df399e533b9043e2712e10c
lola-source <99507617+lola-source@users.noreply.github.com>
2023-02-02 23:11:36 +0300

    Update 0-add.py

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
-""" Basic annotations - add """
+"""
+    Module with typing Python
+"""
 
 
 def add(a: float, b: float) -> float:
-    """ type-annotated function add that takes a float a and a float b
-        as arguments and returns their sum as a float.
     """
-    return a + b
+        Args:
+            a: The first parameter
+            b: The second parameter
+        Return:
+            Sum of two floats
+    """
+
+    return (a + b)
